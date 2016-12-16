abf4d1ab5cbe8bd91ea8ffe3f5dca535d6eaac96
Marcus Hrvatin <marcus@hrvatin.se>
2016-12-16 14:38:41 +0100

    more work done!

--- script1.bash
+++ script1.bash
@@ -13,29 +13,32 @@ function removeDotDot()
 function printLinks()  
 {
 	
-	echo "Links to work with $1"	
-	
+	#echo "Links to work with $1"	
+  #echo "\$1 is: $1"
+  #echo "\$2 is: $2"
+  #echo "\$3 is: $3"
+  full_output="$2 $3"
+  _path=$(echo "$3" | sed -e 's!\(/.*/\).*!\1!')
+  _file=$(echo "$1" | sed -e 's!.*\(/.*/\)\(.*\)!\2!')
 
-	# 
 	if [[ $1 != *"http"* ]] ; then
+    external_domain="$2"
 
 		if [[ $1 != /* ]] ; then
-			#echo "Not starting with a slash, adding a a slash" DEBUGGING ONLY
-			echo "$2/$1"
+
+      full_output+=" $external_domain $_path$_file"
 		fi
 
 		if [[ $1 == /* ]] ; then
-			#echo "Already got a slash, no need to add another" DEBUGGING ONLY
-			echo "$2$1"
-
+      full_output+=" $external_domain $_path$_file"
 		fi
-		
-		#echo "Found a ingoing link: $1i" DEBUGGING ONLY
-		#echo "Adding domain.." DEBUGGING ONLY
-		#echo "$2$1" DEBBUGING ONLY
+  else
+    external_domain="" # parse external domain from string
+  fi
 
-	
-	fi	
+  #echo "$full_output"
+  $(echo "$full_output" >> links_database)
+  #echo "-------------------------"
 }
 
 domain=$1
@@ -47,8 +50,7 @@ output="$($curl $curlargs $rvmhttp)"
 anchor_element=""
 
 $(rm anchors)
-$(rm links)
-
+$(rm links_database)
 
 while read line ; do
   if grep -q "<a" <<< $line ; then
@@ -60,10 +62,8 @@ while read line ; do
 		fi	
     fi
   fi
-  
-
 done <<< "$output"
 
-
-
-
+# TODO fix sorting!
+#$(sort links_database | uniq -u)
+echo $(cat links_database)
